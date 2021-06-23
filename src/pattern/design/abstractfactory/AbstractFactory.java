35800a6c94d6dc2429b765f1a1073d7fef42fce2
shalabhraj1990 <shalabhrajetc@gmail.com>
2021-06-23 07:11:28 +0530

    add method added

--- src/pattern/design/abstractfactory/AbstractFactory.java
+++ src/pattern/design/abstractfactory/AbstractFactory.java
@@ -4,5 +4,11 @@ public class AbstractFactory {
 	public static void main(String[] args) {
 		IRenderUi renderui = new AbstractFactoryCls().getUI(1);
 		System.out.println(renderui.renderUI());
+		
+		
+	}
+	
+	public int add(int a, int b){
+	return a + b;
 	}
 }
